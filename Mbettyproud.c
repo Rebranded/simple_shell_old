42450cf9365cf5584755aefe18de79cafc5db922
Rebranded <akwaowombo@gmail.com>
2023-07-23 10:08:53 -0700

    code thats passes betty test

---
+++ Mbettyproud.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Writing my shell, looking forward to a great adventure,\n");
+	return (0);
+}
